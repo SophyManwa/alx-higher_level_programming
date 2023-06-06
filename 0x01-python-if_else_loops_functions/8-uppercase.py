a64ff4b0176a68646337bf99c7a085ddc53251d4
SophyManwa <sophykwamboka24@gmail.com>
2023-06-06 22:43:40 +0300

    prints string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print(uppercase_char, end='')
-    print()
+        result += uppercase_char
+    print(result)
