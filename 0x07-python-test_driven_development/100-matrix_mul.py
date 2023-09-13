b04968e084d15c7ff63adaf40b7577a8998c587d
SophyManwa <sophykwamboka24@gmail.com>
2023-09-13 22:46:18 +0300

    new commit

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module composed by a function that multiplies 2 matrices
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """ Function that multiplies 2 matrices
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+    Returns:
+        result of the multiplication
+    Raises:
+        TypeError: if m_a or m_b aren't a list
+        TypeError: if m_a or m_b aren't a list of a lists
+        ValueError: if m_a or m_b are empty
+        TypeError: if the lists of m_a or m_b don't have integers or floats
+        TypeError: if the rows of m_a or m_b don't have the same size
+        ValueError: if m_a and m_b can't be multiplied
+    """
+
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    for elems in m_a:
+        if not isinstance(elems, list):
+            raise TypeError("m_a must be a list of lists")
+
